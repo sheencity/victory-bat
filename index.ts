31c48a1b5efda975e78701e7e44504f31afc63da
zhangqiang <350037310@qq.com>
2020-02-06 20:16:18 +0800

    feat: 初步完成单聊和@群聊 功能

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { Wechaty, Friendship, Contact, Message, ScanStatus, Room } from 'wechaty';
 import { ContactSelf } from 'wechaty/dist/src/user';
 import * as qrcodeTerminal from 'qrcode-terminal';
-import { text } from './defaultText';
+import { textBuilder } from './defaultText';
 
 // 延时函数，防止检测出类似机器人行为操作
 const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
@@ -33,34 +33,57 @@ async function onMessage(msg: Message) {
     const contact = msg.from() as Contact; // 发消息人
     const content = msg.text().trim(); // 消息内容
     const room = msg.room(); // 是否是群消息
+    const hasAt = msg.to();
+    console.log(hasAt);
     const hasAtBot = room && (content.includes('@防疫卫士') || content.includes('@Tycho'));
-    const alias = await contact.alias(); // 发消息人备注
+    const alias = await contact.alias() ?? contact.name(); // 发消息人备注
     const isText = msg.type() === bot.Message.Type.Text;
+    console.log(`发消息人: ${contact.name()} 内容: ${content}`);
     if (msg.self()) {
         return;
     }
+
+    // 私聊
+    if (contact.friend() && !room) {
+        console.log(`是 friend`);
+        // 加延迟，防封号
+        await delay(2000);
+        await contact.say(textBuilder(alias));
+    }
+
+    // 群聊
     if (room && isText) {
         // 如果是群消息 目前只处理文字消息
         const topic = await room.topic();
-        console.log(`群名: ${topic} 发消息人: ${contact.name()} 内容: ${content}`);
         if (hasAtBot) {
-            const thisRoom = await Room.find({ id: room.id });
-            await thisRoom?.say(text);
+            console.log(`群名: ${topic} 发消息人: ${contact.name()} 内容: ${content}`);
+            await delay(2000);
+            room.say(textBuilder());
         }
     }
 }
 
+// 加好友
 function onFriendship(friendship: Friendship) {
     friendship.accept();
 }
 
-const bot = new Wechaty({ name: 'WechatEveryDay' });
+// 进群
+async function onRoomJoin(room: Room, inviteeList: Contact[], inviter: Contact, ) {
+    const nameList = inviteeList.map(c => c.name()).join(',')
+    await delay(2000);
+    room.say(textBuilder(nameList));
+    console.log(`Room ${room.topic()} got new member ${nameList}, invited by ${inviter}`)
+}
+
+const bot = new Wechaty({ name: '防疫卫士' });
 
 bot.on('scan', onScan);
 bot.on('login', onLogin);
 bot.on('logout', onLogout);
 bot.on('message', onMessage);
 bot.on('friendship', onFriendship);
+bot.on('room-join', onRoomJoin);
 bot
     .start()
     .then(() => console.log('开始登陆微信'))
